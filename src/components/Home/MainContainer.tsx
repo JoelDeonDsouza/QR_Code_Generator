277e5d3948ae3bd2c3a735c7697ce1e304d8e69d
joeldeondsouza <jdeondsouza@gmail.com>
2023-12-17 19:07:38 +0100

    added ReadME

--- src/components/Home/MainContainer.tsx
+++ src/components/Home/MainContainer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // styled components //
 import {
   Main,
@@ -9,14 +9,23 @@ import {
   InputText,
   ImgCode,
 } from "../../styles/home";
+// QR //
+import QRCode from "qrcode.react";
 
 const MainContainer = () => {
+  const [url, setUrl] = useState("");
+
   return (
     <Main>
       <MainWrapper>
         <TextWrapper>
           <HeaderText>Enter your link</HeaderText>
-          <InputText placeholder="enter your URL" />
+          <InputText
+            type="url"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="enter your URL"
+          />
         </TextWrapper>
         <QrWrapper>
           <ImgCode src={"https://i.ibb.co/kBTf0fZ/qrcode.png"} />
